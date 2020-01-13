8c872771bd411a611ff4e78dad9c7258145906ce
vdonthireddy@gmail.com <N1ravD0n>
2020-01-12 21:59:44 -0800

    Initial check-in of module JWTAuthentication

---
+++ JWTAuthentication/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JWTAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        [Authorize]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var currentUser = HttpContext.User;
+            int spendingTimeWithCompany = 0;
+
+            if (currentUser.HasClaim(c => c.Type == "DateOfJoing"))
+            {
+                DateTime date = DateTime.Parse(currentUser.Claims.FirstOrDefault(c => c.Type == "DateOfJoing").Value);
+                spendingTimeWithCompany = DateTime.Today.Year - date.Year;
+            }
+
+            if (spendingTimeWithCompany > 5)
+            {
+                return new string[] { "More", "Than", "Five", "Years", "old" };
+            }
+            else
+            {
+                return new string[] { "Less", "Than", "five", "years", "old" };
+            }
+        }
+
+    }
+}
---
+++ JWTAuthentication/test.sh
@@ -0,0 +1,7 @@
+﻿curl --location --request POST 'https://localhost:5001/api/login' --header 'Content-Type: application/json' --data-raw '{
+"UserName":"Vijay",
+"Password":"vijay"
+}'
+
+curl --location --request GET 'https://localhost:5001/api/values'
+--header 'Authorization: bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJWaWpheSBEb24iLCJlbWFpbCI6InZkb250aGlyZWRkeUBnbWFpbC5jb20iLCJEYXRlT2ZKb2luZyI6IjIwMTAtMDgtMDIiLCJqdGkiOiIzY2FjY2QyMy1lODY0LTQwMWQtYWI0NS1iZTc3YzBjZTM0NDgiLCJleHAiOjE1Nzg4ODE3NTgsImlzcyI6InNlcnZpY2VjaGFubmVsLmNvbSIsImF1ZCI6InNlcnZpY2VjaGFubmVsLmNvbSJ9.AxmTOLPEG1n2uqHw6EFqOOS82JQjjQfqD0ZRVDrnhVI'
